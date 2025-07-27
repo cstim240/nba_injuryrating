f24def910b7ca1dc80a5169e64e8967857909886
cstim240 <timsupann@gmail.com>
2025-07-27 09:03:26 -0700

    add recovery days for the injury period objects

--- injury_analysis.py
+++ injury_analysis.py
@@ -1,5 +1,5 @@
 import sqlite3
-import datetime
+from datetime import datetime, date 
 
 def getInjuryPeriods(rows):
     # source is a list containing all the rows in the form: [ (name, date, record), ... ,(name, date, record)], we want to turn this into the injury_periods list where the components are separated into a dictionary object, easier to analyze!
@@ -12,12 +12,12 @@ def getInjuryPeriods(rows):
 
     for row in rows:
         name = row[0]
-        date = row[1]
+        current_date = datetime.strptime(row[1], "%Y-%m-%d").date() # from str to datetime obj
         note = row[2].strip().lower()
 
         #case 1: player sustains injury from this season
         if note != "returned to lineup" and name not in currently_injured:
-            currently_injured[name] = (date, note)
+            currently_injured[name] = (current_date, note)
 
         #case 2: player returns from injury from this season
         elif note == "returned to lineup" and name in currently_injured:
@@ -25,8 +25,9 @@ def getInjuryPeriods(rows):
             injury_periods.append({
                 "name": name, 
                 "injury_date": injury_date,
-                "return_date": date,
+                "return_date": current_date,
                 "injury_note": injury_note,
+                "recovery_days": (current_date - injury_date).days
             })
 
         #case 3: player returns from injury from past season
@@ -34,14 +35,16 @@ def getInjuryPeriods(rows):
             continue #skip
     
     #case 4: players who got injured and never returned this season?
-    cutoff_date = datetime.date(2025, 7, 26) #YYYY, MM, DD
+    cutoff_date = date(2025, 7, 26) #YYYY, MM, DD
 
     for name, (injury_date, injury_note) in currently_injured.items():
+        # create an injury period object
         injury_periods.append({
             "name": name, 
             "injury_date": injury_date,
-            "return_date": date,
+            "return_date": current_date,
             "injury_note": injury_note,
+            "recovery_days": (cutoff_date - injury_date).days
         })
     #we give them the arbitrary return date in form of cutoff
 
@@ -66,7 +69,8 @@ def fetchRows():
 
 def main():
     rows = fetchRows()
-    periods = getInjuryPeriods(rows)
+    periods = getInjuryPeriods(rows) # pair injury date and return to lineup into a 'single injury period'
+    print(f"Periods: {periods}")
     # sum the injury duration from each player's periods combined
 
 if __name__ == "__main__":
